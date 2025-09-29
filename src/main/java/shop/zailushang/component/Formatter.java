53d0bd277ff1ec87e72cfdeffd614425ae947ee9
zailushang <631271779@qq.com>
2025-09-29 14:24:54 +0800

    道生异（一），异生万物。（CompletableFuture.completedFuture(1)）；异花异世界，异叶异菩提。（全链路异步，even kick a dog by the roadside）

--- src/main/java/shop/zailushang/component/Formatter.java
+++ src/main/java/shop/zailushang/component/Formatter.java
@@ -3,12 +3,19 @@ package shop.zailushang.component;
 import lombok.extern.slf4j.Slf4j;
 import shop.zailushang.entity.Chapter;
 import shop.zailushang.flow.FlowEngine;
+import shop.zailushang.utils.Assert;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 /**
- * 組件：内容格式化器，调整解密后的章节内容排版、操作包括：用\n 替换 <br/>, 去除首尾空白，拼接章节标题
+ * 組件：内容格式化器，调整解密后的章节内容排版
+ * 操作包括：
+ *      用\n 替换 <br/>
+ *      去除首尾空白
+ *      去除空白行
+ *      拼接章节标题
+ *      章节尾部拼接双换行
  */
 @FunctionalInterface
 public interface Formatter extends Task<Chapter.Chapter4Format, Chapter.Chapter4Write> {
@@ -38,6 +45,7 @@ public interface Formatter extends Task<Chapter.Chapter4Format, Chapter.Chapter4
                     .thenApplyAsync(Chapter.Chapter4Format::unformattedChapterContent, FlowEngine.IO_TASK_EXECUTOR)
                     .thenApplyAsync(unformattedChapterContent -> unformattedChapterContent.replaceAll("<br/>", "\n"), FlowEngine.IO_TASK_EXECUTOR)// 替换换行符
                     .thenApplyAsync(String::lines, FlowEngine.IO_TASK_EXECUTOR)
+                    .thenApplyAsync(stringStream -> stringStream.filter(Assert::strNotBlank), FlowEngine.IO_TASK_EXECUTOR)// 去除空白行
                     .thenApplyAsync(stringStream -> stringStream.map(String::strip), FlowEngine.IO_TASK_EXECUTOR)// 去除行首行尾空格
                     .thenApplyAsync(stringStream -> stringStream.collect(Collectors.joining("\n")), FlowEngine.IO_TASK_EXECUTOR)// 重新拼接换行
                     .thenApplyAsync(chapterContext -> String.format("%s\n%s\n\n", chapter.chapterName(), chapterContext), FlowEngine.IO_TASK_EXECUTOR)// 拼接章节名，行尾添加两个换行符，方便后续文件合并
--- src/main/java/shop/zailushang/utils/Assert.java
+++ src/main/java/shop/zailushang/utils/Assert.java
@@ -1,9 +1,11 @@
 package shop.zailushang.utils;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.regex.Pattern;
 
 /**
  * 断言工具类
@@ -14,53 +16,161 @@ public class Assert {
     private Assert() {
     }
 
+    /**
+     * @param t          待校验参数
+     * @param verifier   参数校验器
+     * @param exSupplier 异常构造器
+     * @param <T>        校验类型
+     * @param <E>        异常类型
+     *                   满足性校验：使用 verifier 传入待校验参数 t,要求参数必须满足校验条件
+     *                   (必须返回 true,当返回 false 时则抛出 exSupplier 制造的异常)
+     */
     public static <T, E extends RuntimeException> void isTrue(T t, Predicate<T> verifier, Supplier<E> exSupplier) {
+        Objects.requireNonNull(verifier, "verifier is null");
+        Objects.requireNonNull(exSupplier, "exSupplier is null");
         // 断言结果
         boolean asserted = verifier.test(t);
-        // 断言未通过
-        if (!asserted) throw exSupplier.get();
+        // 要求断言必须通过
+        ifNotThrow(asserted, exSupplier);
     }
 
-    public static <T, E extends RuntimeException> void isTrue(T caller, T reference, BiPredicate<T, T> verifier, Supplier<E> exSupplier) {
+    /**
+     * @param t          待校验参数
+     * @param verifier   参数校验器
+     * @param exSupplier 异常构造器
+     * @param <T>        校验类型
+     * @param <E>        异常类型
+     *                   不满足性校验：使用 verifier 传入待校验参数 t,要求参数必须不满足校验条件
+     *                   (必须返回 false,当返回 true 时则抛出 exSupplier 制造的异常)
+     */
+    public static <T, E extends RuntimeException> void isFalse(T t, Predicate<T> verifier, Supplier<E> exSupplier) {
+        Objects.requireNonNull(verifier, "verifier is null");
+        Objects.requireNonNull(exSupplier, "exSupplier is null");
         // 断言结果
-        boolean asserted = verifier.test(caller, reference);
-        // 断言未通过
-        if (!asserted) throw exSupplier.get();
+        boolean asserted = verifier.test(t);
+        // 要求断言必须不通过
+        ifThrow(asserted, exSupplier);
     }
 
-    /**********************************************以下为断言时的常用方法,提供静态方法引用 **********************************************/
+    /**
+     * @param left       待校验参数1
+     * @param right      待校验参数2
+     * @param verifier   参数校验器
+     * @param exSupplier 异常构造器
+     * @param <T>        校验参数类型1
+     * @param <U>        校验参数类型2
+     * @param <E>        异常类型
+     *                   满足性校验：使用 verifier 传入待校验参数 left,right,要求参数必须满足校验条件
+     *                   (必须返回 false,当返回 true 时则抛出 exSupplier 制造的异常)
+     */
+    public static <T, U, E extends RuntimeException> void isTrue(T left, U right, BiPredicate<T, U> verifier, Supplier<E> exSupplier) {
+        Objects.requireNonNull(verifier, "verifier is null");
+        Objects.requireNonNull(exSupplier, "exSupplier is null");
+        // 断言结果
+        boolean asserted = verifier.test(left, right);
+        // 要求断言必须通过
+        ifNotThrow(asserted, exSupplier);
+    }
+
+    /**
+     * @param left       待校验参数1
+     * @param right      待校验参数2
+     * @param verifier   参数校验器
+     * @param exSupplier 异常构造器
+     * @param <T>        校验参数类型1
+     * @param <U>        校验参数类型2
+     * @param <E>        异常类型
+     *                   不满足性校验：使用 verifier 传入待校验参数 left,right,要求参数必须满足校验条件
+     *                   (必须返回 false,当返回 true 时则抛出 exSupplier 制造的异常)
+     */
+    public static <T, U, E extends RuntimeException> void isFalse(T left, U right, BiPredicate<T, U> verifier, Supplier<E> exSupplier) {
+        Objects.requireNonNull(verifier, "verifier is null");
+        Objects.requireNonNull(exSupplier, "exSupplier is null");
+        // 断言结果
+        boolean asserted = verifier.test(left, right);
+        // 要求断言必须不通过
+        ifThrow(asserted, exSupplier);
+    }
+
+    /********************************************** 快速抛出异常的方法 **********************************************/
+    public static <E extends RuntimeException> void ifThrow(boolean asserted, Supplier<E> exSupplier) {
+        if (asserted) throw exSupplier.get();
+    }
+
+    public static <E extends RuntimeException> void ifNotThrow(boolean asserted, Supplier<E> exSupplier) {
+        ifThrow(!asserted, exSupplier);
+    }
+
+    /********************************************** 以下为断言时的常用方法,提供静态方法引用 **********************************************/
     // 对象为空
-    public static <T> Boolean isNull(T t) {
+    public static <T> boolean isNull(T t) {
         return Objects.isNull(t);
     }
 
     // 对象非空
-    public static <T> Boolean isNotNull(T t) {
+    public static <T> boolean isNotNull(T t) {
         return Objects.nonNull(t);
     }
 
-    // 比较后相等
-    public static <T extends Comparable<T>> Boolean isEq(T caller, T reference) {
+    // str 非空
+    public static boolean strNotBlank(String str) {
+        return isNotNull(str) && !str.isBlank();
+    }
+
+    // 集合非空
+    public static <T> boolean collectionNotEmpty(Collection<T> collection) {
+        return isNotNull(collection) && !collection.isEmpty();
+    }
+
+    // 数组非空
+    public static <T> boolean arrayNotEmpty(T[] array) {
+        return isNotNull(array) && array.length > 0;
+    }
+
+    // str 正则匹配
+    public static boolean strRgx(String str, String regex) {
+        return isNotNull(str)
+                && isNotNull(regex)
+                && Pattern.matches(regex, str);
+    }
+
+    // 对象相等
+    public static <T, U> boolean isEq(T left, U right) {
+        return Objects.equals(left, right);
+    }
+
+    // 对象不相等
+    public static <T, U> boolean isNotEq(T left, U right) {
+        return !isEq(left, right);
+    }
+
+    // 比较后相等,不做判空校验，逻辑不允许
+    public static <T extends Comparable<T>> boolean isOrderEq(T caller, T reference) {
         return caller.compareTo(reference) == 0;
     }
 
-    // 比较后大于
-    public static <T extends Comparable<T>> Boolean isGt(T caller, T reference) {
-        return caller.compareTo(reference) == 1;
+    // 比较后不相等,不做判空校验，逻辑不允许
+    public static <T extends Comparable<T>> boolean isOrderNotEq(T caller, T reference) {
+        return !isOrderEq(caller, reference);
     }
 
-    // 比较后大于等于
-    public static <T extends Comparable<T>> Boolean isGe(T caller, T reference) {
+    // 比较后大于,不做判空校验，逻辑不允许
+    public static <T extends Comparable<T>> boolean isOrderGt(T caller, T reference) {
+        return caller.compareTo(reference) > 0;
+    }
+
+    // 比较后大于等于,不做判空校验，逻辑不允许
+    public static <T extends Comparable<T>> boolean isOrderGe(T caller, T reference) {
         return caller.compareTo(reference) >= 0;
     }
 
-    // 比较后小于
-    public static <T extends Comparable<T>> Boolean isLt(T caller, T reference) {
-        return caller.compareTo(reference) == -1;
+    // 比较后小于,不做判空校验，逻辑不允许
+    public static <T extends Comparable<T>> boolean isOrderLt(T caller, T reference) {
+        return caller.compareTo(reference) < 0;
     }
 
-    // 比较后小于等于
-    public static <T extends Comparable<T>> Boolean isLe(T caller, T reference) {
+    // 比较后小于等于,不做判空校验，逻辑不允许
+    public static <T extends Comparable<T>> boolean isOrderLe(T caller, T reference) {
         return caller.compareTo(reference) <= 0;
     }
 }
