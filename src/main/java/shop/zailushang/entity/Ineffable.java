2b1d0518767dbabf917d03bad8a431249445756c
zailushang <631271779@qq.com>
2025-10-20 20:53:49 +0800

    有物混成，先天地生，无形无象，无声无臭，混沌谓之「道」。

--- src/main/java/shop/zailushang/entity/Ineffable.java
+++ src/main/java/shop/zailushang/entity/Ineffable.java
@@ -4,4 +4,7 @@ package shop.zailushang.entity;
  * 不可道 ：「道可道，非常道，道之不清，言之不明」
  */
 public interface Ineffable {
+    default void explain() {
+        throw new UnsupportedOperationException("The Tao that can be trodden is not the enduring and unchanging Tao. — Lao Tzu");
+    }
 }
--- src/main/java/shop/zailushang/entity/Tao.java
+++ src/main/java/shop/zailushang/entity/Tao.java
@@ -18,5 +18,15 @@ public final class Tao implements Ineffable {
     public static final Tao CHAOS = null;
 
     private Tao() {
+        // 使我介然有知，行于大道，唯施是畏。大道甚夷，而人好径。朝甚除，田甚芜，仓甚虚，服文采，带利剑，厌饮食，财货有余，是谓盗竽。非道也哉！
+        throw new UnsupportedOperationException("""
+                If I have the least knowledge of the Way,
+                I walk along the great Path, fearing only to stray.
+                The great Path is very smooth, yet people love byways.
+                The court is utterly corrupt, the fields are utterly wild, the granaries are utterly empty;
+                Yet some wear gorgeous clothes, carry sharp swords, glut themselves with food and drink,
+                And have more possessions than they can use!
+                They are the heralds of robbery. How far from the Way they have strayed!
+                """);
     }
 }
